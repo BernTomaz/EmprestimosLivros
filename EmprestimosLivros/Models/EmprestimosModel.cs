7cd8778f26df7d26f6b58e52a5fa6eb0245113ed
Bernardo Tomaz <99764547+BernTomaz@users.noreply.github.com>
2025-01-10 17:15:43 -0300

    Inclusão da view, model e controller da classe Emprestimo.

---
+++ EmprestimosLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EmprestimosLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+
+        public DateTime DataUltimaAtualização { get; set; } = DateTime.Now;
+    }
+}
