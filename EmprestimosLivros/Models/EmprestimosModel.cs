506fa03708a9a5babd68fabad8b0ff1936e03cd1
Bernardo Tomaz <99764547+BernTomaz@users.noreply.github.com>
2025-01-19 17:40:24 -0300

    feat(emprestimos): adicionar views e ajustar lógica para cadastro, edição e exclusão
    - Criadas as views Cadastrar, Editar e Excluir para a funcionalidade de empréstimos
    - Ajustada a lógica de exclusão no EmprestimoController
    - Atualizado o modelo EmprestimosModel
    - Melhorias no layout da página inicial (Index.cshtml)

--- EmprestimosLivros/Models/EmprestimosModel.cs
+++ EmprestimosLivros/Models/EmprestimosModel.cs
@@ -1,10 +1,18 @@
-﻿namespace EmprestimosLivros.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimosLivros.Models
 {
     public class EmprestimosModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O recebedor é obrigatório.")]
         public string Recebedor { get; set; }
+
+        [Required(ErrorMessage = " O fornecedor é obrigatório.")]
         public string Fornecedor { get; set; }
+
+        [Required(ErrorMessage = "O nome do livro é obrigatório.")]
         public string LivroEmprestado { get; set; }
 
         public DateTime DataUltimaAtualização { get; set; } = DateTime.Now;
